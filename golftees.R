652799130022da4948c1d4ba069aea1ea6d87a65
david-borchers <dlb@st-andrews.ac.uk>
2022-05-09 13:00:18 +0100

    added two figures

--- golftees.R
+++ golftees.R
@@ -1,4 +1,6 @@
 gex.data=read.csv("gex.data")
+
+pdf(file="./figures/teeplot.pdf",h=6, w=10)
 attach(gex.data)
 plot(east,north,cex=s/2,col=gray(1-(v+1)/max(v+1)),pch=19-c) #s is group size, v is visibility (0/1), c is color (green/yellow, referred to as "sex" in the book)
 detach(gex.data)
@@ -9,7 +11,10 @@ gstrips=read.csv("glines")
 gbnd=read.csv("gbnd")
 detach(glines)
 attach(gbnd)
-for(i in 1:(length(gbnd[,1])-1)) lines(c(x[i],x[i+1]),c(y[i],y[i+1]),col=type[i]*Stratum[i]+1,lwd=2)
+for(i in 1:(length(gbnd[,1])-1)) lines(c(x[i],x[i+1]),c(y[i],y[i+1]),col=type[i]*Stratum[i]+1,lwd=2,lty=2)
+dev.off()
 
 seen = apply(gex.data[,16:23],1,sum) # number times seen by anyone
-plot(abs(gex.data$SignPerp),seen>0) # seen at all vs perp dist
+pdf(file="./figures/seenplot.pdf",h=6, w=10)
+plot(abs(gex.data$SignPerp),seen>0,xlab="Perpendicular distance",ylab="Seen") # seen at all vs perp dist
+dev.off()
